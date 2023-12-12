70ba812436d8503a3e91c08c0d183b57c241c887
mferguson8 <125309694+mferguson8@users.noreply.github.com>
2023-12-12 10:34:20 -0500

    resorted and added game list

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 class User
@@ -17,14 +18,26 @@ private:
     string password;
 };
 
+class Game {
+public:
+    string opponent;
+    string location;
+    int wins;
+    int losses;
+};
+
 void createAccount(User& user); // Function prototype
 void login(User& user); // Function prototype
 void displayAccount(User& user); // Function prototype
 void calculateWinPercentage(User& user); // Function prototype
+void makeGame(User& user, vector<Game>& games); // Function prototype
+void viewAllGames(const vector<Game>& games); // Function prototype
 
 int main()
 {
-    User currentUser;
+    User user;
+    vector<Game> games;
+
     int choice;
     bool loggedIn = false;
 
@@ -38,18 +51,55 @@ int main()
     if (choice == 1)
     {
         loggedIn = true;
-        createAccount(currentUser);
-        displayAccount(currentUser);
+        createAccount(user);
+        displayAccount(user);
     }
     else if (choice == 2)
     {
         loggedIn = true;
-        login(currentUser);
-        displayAccount(currentUser);
+        login(user);
+        displayAccount(user);
     }
     else
     {
         cout << "Invalid choice!" << endl;
+    }
+
+    while (loggedIn)
+    {
+        cout << "Please choose an option:" << endl;
+        cout << "1. View Stats" << endl;
+        cout << "2. Calculate Win %" << endl;
+        cout << "3. Make a Game" << endl;
+        cout << "4. View All Games" << endl;
+        cout << "5. Logout" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if (choice == 1)
+        {
+            displayAccount(user);
+        }
+        else if (choice == 2)
+        {
+            calculateWinPercentage(user);
+        }
+        else if (choice == 3)
+        {
+            makeGame(user, games);
+        }
+        else if (choice == 4)
+        {
+            viewAllGames(games);
+        }
+        else if (choice == 5)
+        {
+            loggedIn = false;
+        }
+        else
+        {
+            cout << "Invalid choice!" << endl;
+        }
     }
 
     return 0;
@@ -106,39 +156,6 @@ void login(User& user)
     }
 }
 
-void homeScreen(User& user){
-    int choice;
-
-    cout << "Welcome to the Faceoff App!" << endl;
-    cout << "Please choose an option:" << endl;
-    cout << "1. View Stats" << endl;
-    cout << "2. Calculate Win %" << endl;
-    cout << "3. Update Stats" << endl;
-    cout << "4. Add a Game" << endl;
-    cin >> choice;
-
-    if (choice == 1)
-    {
-        displayAccount(user);
-    }
-    else if (choice == 2)
-    {
-        calculateWinPercentage(user);
-    }
-    else if (choice == 3)
-    {
-        updateStats(user);
-    }
-    else if (choice == 4)
-    {
-        addGame(user);
-    }
-    else
-    {
-        cout << "Invalid choice!" << endl;
-    }
-}
-
 void displayAccount(User& user)
 {
     cout << "Team: " << user.team << endl;
@@ -158,15 +175,10 @@ void calculateWinPercentage(User& user)
     cout << "Win Percentage: " << winPercentage << endl;
 }
 
-class game(){
-    public:
-        string opponent;
-        string location;
-        int wins;
-        int losses;
-};
+void makeGame(User& user, vector<Game>& games)
+{
+    Game game;
 
-void makeGame(User& user, game& game){
     cout << "Enter opponent: ";
     cin >> game.opponent;
 
@@ -182,7 +194,19 @@ void makeGame(User& user, game& game){
     user.totalWins += game.wins;
     user.totalLosses += game.losses;
 
-    double winPercentage = calculateWinPercentage(user);
+    games.push_back(game);
 
     cout << "Game added!" << endl;
+}
+
+void viewAllGames(const vector<Game>& games)
+{
+    for (const auto& game : games)
+    {
+        cout << "Opponent: " << game.opponent << endl;
+        cout << "Location: " << game.location << endl;
+        cout << "Wins: " << game.wins << endl;
+        cout << "Losses: " << game.losses << endl;
+        cout << endl;
+    }
 }
